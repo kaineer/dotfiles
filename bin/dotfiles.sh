8bb46a94cb2837810a6264f3864f0eb28c2b5d69
Sergey Kluchkovsky <kaineer@gmail.com>
2024-07-25 20:06:45 +0500

    Remove dconf from dotfiles.sh

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -54,21 +54,12 @@ play_ansible_playbook() {
     ansible-playbook local.yml
 }
 
-dconf_settings() {
-  dconf write "/org/gnome/terminal/legacy/default-show-menubar" false
-  dconf write "/org/gnome/terminal/legacy/always-check-default-menubar" false
-
-  dconf write "/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/font" "'FiraCode Nerd Font 15'"
-  dconf write "/org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/palette" "['#3B3B42425252', '#BFBF61616A6A', '#A3A3BEBE8C8C', '#EBEBCBCB8B8B', '#8181A1A1C1C1', '#B4B48E8EADAD', '#8888C0C0D0D0', '#E5E5E9E9F0F0', '#4C4C56566A6A', '#BFBF61616A6A', '#A3A3BEBE8C8C', '#EBEBCBCB8B8B', '#8181A1A1C1C1', '#B4B48E8EADAD', '#8F8FBCBCBBBB', '#ECECEFEFF4F4']"
-}
-
 main() {
   ensure_git
   ensure_ansible
   ensure_ssh_keys
   install_ansible_roles
   play_ansible_playbook
-  # dconf_settings
 }
 
 main
