b9ff47a5e329f02ef5ec26a30fc08b187056c245
Sergey Kluchkovsky <kaineer@gmail.com>
2024-12-19 13:13:17 +0500

    Save

--- ansible/files/wezterm/shell.lua
+++ ansible/files/wezterm/shell.lua
@@ -3,5 +3,6 @@
 -- @link: [Change default shell](https://wezfurlong.org/wezterm/config/launch.html#changing-the-default-program)
 --
 return function(config)
-  config.default_prog = { '/bin/bash', '-i' }
+  -- config.default_prog = { '/bin/bash', '-i' }
+  config.default_prog = { '/usr/bin/tmux' }
 end
