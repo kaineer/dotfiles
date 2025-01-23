d33036d202f12d5dbb65680a25863731f490c008
Sergey Kluchkovsky <kaineer@gmail.com>
2025-01-23 11:51:28 +0500

    Sync from teddy at 23.01.2025 -- 11:51

--- ansible/files/nvim/config/lua/plugins/eyeliner/init.lua
+++ ansible/files/nvim/config/lua/plugins/eyeliner/init.lua
@@ -6,5 +6,8 @@ return {
       highlight_on_key = true, -- show highlights only after keypress
       dim = true,              -- dim all other characters if set to true (recommended!)
     }
+
+    vim.api.nvim_set_hl(0, 'EyelinerPrimary', { fg='#000000', bold = true, underline = true })
+    vim.api.nvim_set_hl(0, 'EyelinerSecondary', { fg='#ffffff', underline = true })
   end
 }
