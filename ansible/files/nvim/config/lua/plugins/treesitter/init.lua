3233aa891498cc37064b222a0db3682e2f3d36d3
Sergey Kluchkovsky <kaineer@gmail.com>
2025-01-23 16:28:43 +0500

    Sync from teddy at 23.01.2025 -- 16:28

--- ansible/files/nvim/config/lua/plugins/treesitter/init.lua
+++ ansible/files/nvim/config/lua/plugins/treesitter/init.lua
@@ -1,33 +1,29 @@
---
+-- plugins/treesitter/init.lua
+
+local languages = {
+	"bash",
+	"yaml",
+	"lua",
+	"http",
+	"hurl",
+	"cpp",
+	"python",
+	"ruby",
+	"rust",
+	"make",
+	"tsx",
+	"javascript",
+	"vimdoc",
+}
 
 return {
-  "nvim-treesitter/nvim-treesitter",
-  -- run = function()
-  --   local ts_update = require('nvim-treesitter.install').update({
-  --     with_sync = true
-  --   })
-  --   ts_update()
-  -- end,
-  config = function()
-    require("nvim-treesitter.configs").setup {
-      indent = {
-        enable = true,
-      },
-      ensure_installed = {
-        "bash",
-        "yaml",
-        "lua",
-        "http",
-        "hurl",
-        "cpp",
-        "python",
-        "ruby",
-        "rust",
-        "make",
-        "tsx",
-        "javascript",
-        "vimdoc",
-      }
-    }
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			indent = {
+				enable = true,
+			},
+			ensure_installed = languages,
+		})
+	end,
 }
