fc121d1db73e942b185c0ff6469ff99324d0620e
Sergey Kluchkovsky <kaineer@gmail.com>
2025-01-27 06:52:13 +0500

    Sync from hyouka at 27.01.2025 -- 06:52

--- ansible/files/nvim/old-config/lua/plugins/editing/snippets.lua
+++ ansible/files/nvim/old-config/lua/plugins/editing/snippets.lua
@@ -1,14 +1,14 @@
 --
 return {
-  "garymjr/nvim-snippets",
+	"garymjr/nvim-snippets",
 
-  config = function ()
-    require("snippets").setup {
-      create_cmp_source = true,
-      friendly_snippets = true,
-      extended_filetypes = {
-        typescript = { 'javascript' },
-      },
-    }
-  end,
+	config = function()
+		require("snippets").setup({
+			create_cmp_source = true,
+			friendly_snippets = true,
+			extended_filetypes = {
+				typescript = { "javascript" },
+			},
+		})
+	end,
 }
