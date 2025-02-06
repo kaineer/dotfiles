bfe4ff8d397421c84a17f0b510b317703fdae085
Sergey Kluchkovsky <kaineer@gmail.com>
2025-02-06 12:43:21 +0500

    Sync from teddy at 06.02.2025 -- 12:43

--- ansible/files/nvim/config/lua/autocmd/terminal.lua
+++ ansible/files/nvim/config/lua/autocmd/terminal.lua
@@ -1,12 +1,12 @@
 return {
-  name = "Terminal",
-  config = function(cmd)
-    cmd("TermOpen", {
-      pattern = "*",
-      callback = function()
-        vim.cmd(":startinsert")
-        vim.cmd("setlocal listchars= nonumber norelativenumber")
-      end,
-    })
-  end,
+	name = "Terminal",
+	config = function(cmd)
+		cmd("TermOpen", {
+			pattern = "*",
+			callback = function()
+				vim.cmd(":startinsert")
+				vim.cmd(":setlocal listchars= nonumber norelativenumber")
+			end,
+		})
+	end,
 }
--- ansible/files/nvim/config/lua/plugins/lualine/init.lua
+++ ansible/files/nvim/config/lua/plugins/lualine/init.lua
@@ -4,9 +4,8 @@ return {
 		local mode = {
 			"mode",
 			fmt = function(str)
-				return str:sub(1, 1)
-				-- return " " .. str
-				-- return ' ' .. str:sub(1, 1) -- displays only the first character of the mode
+				-- return " " .. str:sub(1, 1) -- displays only the first character of the mode
+				return " " .. str
 			end,
 		}
 
