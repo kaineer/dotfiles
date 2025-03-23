05a178279b7e73fa89ea6c0f4667fa424766bc73
Sergey Kluchkovsky <kaineer@gmail.com>
2025-03-23 16:46:42 +0500

    Sync from hyouka at 23.03.2025 -- 16:46

--- ansible/files/nvim/config/lua/plugins/completion/setup.lua
+++ ansible/files/nvim/config/lua/plugins/completion/setup.lua
@@ -82,4 +82,6 @@ return function()
     imap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
     smap <expr> <C-j>   vsnip#expandable()  ? '<Plug>(vsnip-expand)'         : '<C-j>'
   ]])
+
+	vim.g.vsnip_snippet_dir = vim.fn.expand("~/.config/snippets/")
 end
