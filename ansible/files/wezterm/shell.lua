ea8b26d860976eb7ac40e5f9a993573477256a22
Sergey Kluchkovsky <kaineer@gmail.com>
2025-04-17 11:40:33 +0500

    Sync from teddy at 17.04.2025 -- 11:40

--- ansible/files/wezterm/shell.lua
+++ ansible/files/wezterm/shell.lua
@@ -3,7 +3,6 @@
 -- @link: [Change default shell](https://wezfurlong.org/wezterm/config/launch.html#changing-the-default-program)
 --
 return function(config)
-	local user = os.getenv("USER")
 	config.default_prog = { "/usr/bin/bash", "-i" }
-	config.default_cwd = "/home/" .. user
+	config.default_cwd = os.getenv("HOME")
 end
