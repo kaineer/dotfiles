2132545d596cc584148cb61d75ce1db1604da231
Sergey Kluchkovsky <kaineer@gmail.com>
2025-08-13 10:09:46 +0500

    Sync from teddy at 13.08.2025 -- 10:09

--- ansible/files/lf/previewer.sh
+++ ansible/files/lf/previewer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 preview_code() {
-  if [[ "$(which bat)" == "" ]]
-  then
+  if [[ "$(which bat)" == "" ]]; then
     cat $1 | head -n $2
   else
     bat $1 -p -f -r ":$2"
@@ -10,12 +9,22 @@ preview_code() {
 }
 
 preview_video() {
-  if [[ "$(which mediainfo)" == "" ]]
-  then
+  if [[ "$(which mediainfo)" == "" ]]; then
     echo -e "Some video file.\n\nInstall «mediainfo» to see more"
   else
     mediainfo "$1" | head -n $2
   fi
+}
+
+preview_image() {
+  /bin/img2txt "$1" | head -n $2
+  # if [[ "$(which img2txt)" != "" ]]; then
+  #   img2txt "$1" | head -n $2
+  # elif [[ "$(which mediainfo)" != "" ]]; then
+  #   mediainfo "$1" | head -n $2
+  # else
+  #   echo -e "Some video file.\n\nInstall «mediainfo» to see more"
+  # fi
 }
 
 preview_binary() {
@@ -25,19 +34,21 @@ preview_binary() {
 FILENAME="$1"
 TERMINAL_HEIGHT="$2"
 
-if [ -f "$1" ]
-then
+if [ -f "$1" ]; then
   FILETYPE=$(eval "file --mime-type ${FILENAME// /\\\ } -b")
   case $FILETYPE in
-    text/*|application/json)
-      preview_code "$FILENAME" $TERMINAL_HEIGHT
-      ;;
-    video/*|image/*)
-      preview_video "$FILENAME" $TERMINAL_HEIGHT
-      ;;
-    *)
-      preview_binary "$FILENAME" $TERMINAL_HEIGHT
-      ;;
+  text/* | application/json)
+    preview_code "$FILENAME" $TERMINAL_HEIGHT
+    ;;
+  image/*)
+    preview_image "$FILENAME" $TERMINAL_HEIGHT
+    ;;
+  video/*)
+    preview_video "$FILENAME" $TERMINAL_HEIGHT
+    ;;
+  *)
+    preview_binary "$FILENAME" $TERMINAL_HEIGHT
+    ;;
   esac
 else
   exa -l $*
