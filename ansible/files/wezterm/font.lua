8440fc3a201ff43d03b8be5fbebe03eaba8629c8
Sergey Kluchkovsky <kaineer@gmail.com>
2025-09-25 14:42:52 +0500

    Sync from teddy at 25.09.2025 -- 14:42

--- ansible/files/wezterm/font.lua
+++ ansible/files/wezterm/font.lua
@@ -1,7 +1,7 @@
 -- font.lua
 
 return function(config, wezterm)
-  config.font_size = 16.0
+  config.font_size = 25.0
 
   -- To get available font names use
   --
@@ -9,8 +9,6 @@ return function(config, wezterm)
   --
   config.font = wezterm.font_with_fallback({
     "FiraCode Nerd Font", -- firacode, it is more compact
-    "GitLab Mono",        -- gitlab eyecandy
+    "GitLab Mono", -- gitlab eyecandy
   })
-
-  config.warn_about_missing_glyphs = false
 end
