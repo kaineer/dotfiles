40ebe33bba4bbb9b272768aeeb46aeb6fb476987
Sergey Kluchkovsky <kaineer@gmail.com>
2025-11-20 21:36:54 +0500

    Sync from myrkur at 20.11.2025 -- 21:36

--- ansible/files/nvim/config/lua/plugins/colors/onenord.lua
+++ ansible/files/nvim/config/lua/plugins/colors/onenord.lua
@@ -3,11 +3,12 @@
 -- gh: https://github.com/rmehri01/onenord.nvim
 --
 return {
-  "rmehri01/onenord.nvim",
-  config = function()
-    require("onenord").setup({
-      theme = "dark",
-      fade_nc = true,
-    })
-  end,
+	"rmehri01/onenord.nvim",
+	config = function()
+		require("onenord").setup({
+			theme = "dark",
+			fade_nc = true,
+		})
+		vim.cmd([[ highlight Normal guibg=none ]])
+	end,
 }
