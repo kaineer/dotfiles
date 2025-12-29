cf2034a33cd07e0d114cd73544bff4f927d1529a
Sergey Kluchkovsky <kaineer@gmail.com>
2025-12-29 20:19:03 +0500

    Sync from myrkur at 29.12.2025 -- 20:19

--- ansible/files/nvim/config/init.lua
+++ ansible/files/nvim/config/init.lua
@@ -17,16 +17,21 @@ require("lazy").setup({
 	require("plugins.edit"),
 	require("plugins.lsp"),
 	require("plugins.none-ls"),
-	require("plugins.lualine"),
+	-- require("plugins.lualine"),
 	require("plugins.telekasten"),
+
+	{
+		dir = "~/devel/kaineer/skk-lua.nvim",
+		config = function() end,
+	},
 
 	-- require("plugins.skk.plugin"),
 
-	{
-		"kaineer/skk",
-		dir = "~/.config/nvim/lua/plugins/skk",
-		config = function()
-			require("plugins.skk").setup()
-		end,
-	},
+	-- {
+	-- 	"kaineer/skk",
+	-- 	dir = "~/.config/nvim/lua/plugins/skk",
+	-- 	config = function()
+	-- 		require("plugins.skk").setup()
+	-- 	end,
+	-- },
 })
