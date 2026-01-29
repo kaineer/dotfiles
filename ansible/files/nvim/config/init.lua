ba6e682918f9060a3d2217eafcf2245262332aa9
Sergey Kluchkovsky <kaineer@gmail.com>
2026-01-29 11:20:34 +0500

    Sync from teddy at 29.01.2026 -- 11:20

--- ansible/files/nvim/config/init.lua
+++ ansible/files/nvim/config/init.lua
@@ -6,32 +6,32 @@ require("core.keymaps")
 
 -- setup plugins
 require("lazy").setup({
-	require("plugins.colors.nord"),
-	-- require("plugins.colors.vague"),
-	require("plugins.telescope"),
-	require("plugins.navigation"),
-	require("plugins.treesitter"),
-	require("plugins.git"),
-	require("plugins.languages"),
-	require("plugins.completion"),
-	require("plugins.edit"),
-	require("plugins.lsp"),
-	require("plugins.none-ls"),
-	-- require("plugins.lualine"),
-	require("plugins.telekasten"),
+  require("plugins.colors.nord"),
+  -- require("plugins.colors.vague"),
+  require("plugins.telescope"),
+  require("plugins.navigation"),
+  require("plugins.treesitter"),
+  require("plugins.git"),
+  require("plugins.languages"),
+  require("plugins.completion"),
+  require("plugins.edit"),
+  require("plugins.lsp"),
+  require("plugins.none-ls"),
+  -- require("plugins.lualine"),
+  require("plugins.telekasten"),
 
-	{
-		dir = "~/devel/kaineer/skk-lua.nvim",
-		config = function() end,
-	},
+  -- {
+  -- 	dir = "~/devel/kaineer/skk-lua.nvim",
+  -- 	config = function() end,
+  -- },
 
-	-- require("plugins.skk.plugin"),
+  -- require("plugins.skk.plugin"),
 
-	-- {
-	-- 	"kaineer/skk",
-	-- 	dir = "~/.config/nvim/lua/plugins/skk",
-	-- 	config = function()
-	-- 		require("plugins.skk").setup()
-	-- 	end,
-	-- },
+  -- {
+  -- 	"kaineer/skk",
+  -- 	dir = "~/.config/nvim/lua/plugins/skk",
+  -- 	config = function()
+  -- 		require("plugins.skk").setup()
+  -- 	end,
+  -- },
 })
