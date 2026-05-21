77b0061937351b47332ca81171c99f2a8417642d
Sergey Kluchkovsky <kaineer@gmail.com>
2026-05-21 22:02:05 +0500

    Sync from myrkur at 21.05.2026 -- 22:02

--- ansible/files/nvim/config/init.lua
+++ ansible/files/nvim/config/init.lua
@@ -4,35 +4,6 @@ require("core.options")
 require("core.keymaps")
 -- require("autocmd")
 
--- setup plugins
-require("lazy").setup({
-  require("plugins.colors.nord"),
-  -- require("plugins.colors.vague"),
-  require("plugins.telescope"),
-  require("plugins.navigation"),
-  require("plugins.treesitter"),
-  require("plugins.git"),
-  require("plugins.languages"),
-  require("plugins.completion"),
-  require("plugins.edit"),
-  require("plugins.lsp"),
-  require("plugins.none-ls"),
-  require("plugins.lualine"),
-  require("plugins.telekasten"),
-  require("plugins.ai.gen"),
+require("core.vim-pack")
 
-  -- {
-  -- 	dir = "~/devel/kaineer/skk-lua.nvim",
-  -- 	config = function() end,
-  -- },
-
-  -- require("plugins.skk.plugin"),
-
-  -- {
-  -- 	"kaineer/skk",
-  -- 	dir = "~/.config/nvim/lua/plugins/skk",
-  -- 	config = function()
-  -- 		require("plugins.skk").setup()
-  -- 	end,
-  -- },
-})
+-- require("core.lazy")
