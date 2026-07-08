a67379c14fc2faed4bb8d9dc7b79e6edc642ceda
Sergey Kluchkovsky <kaineer@gmail.com>
2026-07-08 21:39:40 +0500

    Sync from myrkur at 08.07.2026 -- 21:39

--- ansible/files/nvim/config/lua/autocmd/focus.lua
+++ ansible/files/nvim/config/lua/autocmd/focus.lua
@@ -1,16 +1,16 @@
 return {
   name = "FocusColors",
   config = function(cmd)
-    cmd("FocusGained", {
-      -- callback = function()
-      --   vim.cmd([[ highlight Normal guibg=#3b4252 ]])
-      -- end,
-    })
-
-    cmd("FocusLost", {
-      -- callback = function()
-      --   vim.cmd([[ highlight Normal guibg=#2e3440 ]])
-      -- end,
-    })
+    -- cmd("FocusGained", {
+    --   callback = function()
+    --     vim.cmd([[ highlight Normal guibg=#3b4252 ]])
+    --   end,
+    -- })
+    --
+    -- cmd("FocusLost", {
+    --   callback = function()
+    --     vim.cmd([[ highlight Normal guibg=#2e3440 ]])
+    --   end,
+    -- })
   end,
 }
