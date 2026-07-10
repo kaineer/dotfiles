f790ad572f6ff5b8ab7905c07bfe2ca357b05ce1
Sergey Kluchkovsky <kaineer@gmail.com>
2026-07-10 17:00:46 +0500

    Sync from teddy at 10.07.2026 -- 17:00

--- ansible/files/nvim/config/lua/autocmd/focus.lua
+++ ansible/files/nvim/config/lua/autocmd/focus.lua
@@ -1,5 +1,16 @@
 return {
   name = "FocusColors",
   config = function(cmd)
+    cmd("FocusGained", {
+      callback = function()
+        vim.cmd([[ highlight Normal guibg=#2e3440 ]])
+      end,
+    })
+
+    cmd("FocusLost", {
+      callback = function()
+        vim.cmd([[ highlight Normal guibg=#3b4252 ]])
+      end,
+    })
   end,
 }
--- ansible/files/nvim/config/lua/plugins/colors/nord.lua
+++ ansible/files/nvim/config/lua/plugins/colors/nord.lua
@@ -7,8 +7,8 @@ return {
   url = "https://github.com/shaunsingh/nord.nvim",
   setup = function()
 		vim.cmd([[ colorscheme nord ]])
-		-- vim.cmd([[ highlight Normal guibg=#2e3440 ]])
-		-- vim.cmd([[ highlight NormalNC guibg=#3b4252 ]])
+		vim.cmd([[ highlight Normal guibg=#2e3440 ]])
+		vim.cmd([[ highlight NormalNC guibg=#3b4252 ]])
 		--
 		-- vim.cmd([[ highlight GitGutterAdd guibg=#2e3440 ]])
 		-- vim.cmd([[ highlight GitGutterChange guibg=#2e3440 ]])
