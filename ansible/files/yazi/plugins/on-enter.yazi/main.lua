73a955e6feb0b91f32b6c28e6ac7291091b9581a
Sergey Kluchkovsky <kaineer@gmail.com>
2026-09-18 16:30:21 +0500

    Sync from teddy at 18.09.2026 -- 16:30

--- ansible/files/yazi/plugins/on-enter.yazi/main.lua
+++ ansible/files/yazi/plugins/on-enter.yazi/main.lua
@@ -2,16 +2,15 @@
 return {
 	entry = function()
 		local h = cx.active.current.hovered
-		if not h then
-			return
-		end
 
-		if h.link_to then
-			ya.emit("follow", {})
-		elseif h.cha.is_dir then
-			ya.emit("enter", {})
-		else
-			ya.emit("open", { hovered = true })
-		end
+    if h then
+		  if h.link_to then
+		  	ya.emit("follow", {})
+		  elseif h.cha.is_dir then
+		  	ya.emit("enter", {})
+		  else
+		  	ya.emit("open", { hovered = true })
+		  end
+    end
 	end,
 }
